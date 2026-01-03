83e726546c0e7241956b59763a85e08cf7bee03f
Jamie Falconer <mrfluffyoven@gmail.com>
2026-01-03 08:25:23 +0000

    hmd > nokia

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/hmd/SCW_sprout
+DEVICE_PATH := device/nokia/SCW_sprout
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
