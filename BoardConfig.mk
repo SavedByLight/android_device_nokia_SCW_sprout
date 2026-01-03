ee03580c6cc35c966617d1c7e8d33a7a6e9b4ebd
Jamie Falconer <mrfluffyoven@gmail.com>
2026-01-03 11:37:01 +0000

    add cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,9 +69,12 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a75
 # APEX
 OVERRIDE_TARGET_FLATTEN_APEX := true
 
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x04C8C000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=4e00000.dwc3 swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket buildvariant=eng
+
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 3
 BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_KERNEL_CONFIG := SCW_sprout_defconfig
